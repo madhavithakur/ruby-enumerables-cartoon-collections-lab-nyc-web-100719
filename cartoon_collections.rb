74f85dfc3cc994c1e23d519ed3bcf9726c7901b2
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-10-05 05:18:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,51 @@ def summon_captain_planet(array)
   array.map {|ele| (ele[0].upcase + ele[1..-1] + "!")}
 end 
 
+<<<<<<< HEAD
+=======
+# describe "#summon_captain_planet" do
+#     it "returns an array with the same number of elements that it was given" do
+#       veggies = ["carrot", "cucumber", "pepper"]
+#       result = summon_captain_planet(veggies)
+#       expect(result.class).to eq(Array)
+#       expect(result.length).to eq(3)
+#     end
+
+#     it "capitalizes each element and adds an exclamation mark (test 1)" do
+#       fruits = ["apple", "banana", "orange"]
+#       result = summon_captain_planet(fruits)
+#       ['Apple!', 'Banana!', 'Orange!'].each { |w| expect(result).to include w }
+#     end
+
+#     it "capitalizes each element and adds an exclamation mark (test 2)" do
+#       veggies = ["carrot", "cucumber", "pepper"]
+#       result = summon_captain_planet(veggies)
+#       ["Carrot!", "Cucumber!", "Pepper!"].each { |w| expect(result).to include w }
+#     end
+#   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def summon_captain_planet# code an argument here
+  # Your code here
+end
+>>>>>>> 9580b817c757b86a714953b8419988ef2441157a
 
 def long_planeteer_calls(array)
   array.each do |word|
